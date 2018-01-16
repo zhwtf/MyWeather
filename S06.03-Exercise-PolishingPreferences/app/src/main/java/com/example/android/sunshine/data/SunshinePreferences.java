bbd9b36390dd7492a89300fb6e6ef1128b327bd0
haozheng <hza89@sfu.ca>
2018-01-15 20:58:48 -0800

    aa

--- S06.03-Exercise-PolishingPreferences/app/src/main/java/com/example/android/sunshine/data/SunshinePreferences.java
+++ S06.03-Exercise-PolishingPreferences/app/src/main/java/com/example/android/sunshine/data/SunshinePreferences.java
@@ -16,6 +16,10 @@
 package com.example.android.sunshine.data;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.example.android.sunshine.R;
 
 public class SunshinePreferences {
 
@@ -88,8 +92,15 @@ public class SunshinePreferences {
      */
     public static String getPreferredWeatherLocation(Context context) {
         // TODO (1) Return the user's preferred location
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+
+        String keyForLocation = context.getString(R.string.pref_location_key);
+
+        String defaultLocation = context.getString(R.string.pref_location_default);
+
+        return prefs.getString(keyForLocation, defaultLocation);
         /** This will be implemented in a future lesson **/
-        return getDefaultWeatherLocation();
+        //return getDefaultWeatherLocation();
     }
 
     /**
@@ -101,8 +112,24 @@ public class SunshinePreferences {
      */
     public static boolean isMetric(Context context) {
         // TODO (2) Return true if the user's preference for units is metric, false otherwise
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+
+        String keyForUnits = context.getString(R.string.pref_units_key);
+
+        String defaultUnits = context.getString(R.string.pref_units_metric);
+
+        String preferredUnits = prefs.getString(keyForUnits, defaultUnits);
+
+        String metric = context.getString(R.string.pref_units_metric);
+
+        boolean userPrefersMetric;
+        if (metric.equals(preferredUnits)) {
+            userPrefersMetric = true;
+        } else {
+            userPrefersMetric = false;
+        }
         /** This will be implemented in a future lesson **/
-        return true;
+        return userPrefersMetric;
     }
 
     /**
