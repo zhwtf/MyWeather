b54733c89a8f261a93fa3c2bd5de9e00cfd56ce6
haozheng <hza89@sfu.ca>
2018-01-19 16:20:44 -0800

    complete notification

--- S10.04-Exercise-Notifications/app/src/main/java/com/example/android/sunshine/data/SunshinePreferences.java
+++ S10.04-Exercise-Notifications/app/src/main/java/com/example/android/sunshine/data/SunshinePreferences.java
@@ -212,4 +212,18 @@ public final class SunshinePreferences {
         editor.putLong(lastNotificationKey, timeOfNotification);
         editor.apply();
     }
+
+    public static boolean areNotificationsEnabled(Context context) {
+        String displayNotificationsKey = context.getString(R.string.pref_enable_notifications_key);
+
+        boolean shouldDisplayNotificationsByDefault = context.getResources().getBoolean(R.bool.show_notifications_by_default);
+
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+
+        boolean shouldDisplayNotifications = sp.getBoolean(displayNotificationsKey, shouldDisplayNotificationsByDefault);
+
+        return shouldDisplayNotifications;
+    }
+
+
 }
